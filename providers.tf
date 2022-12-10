fe1e0e7ccf31cfd118c7332a2da46e9a4f01c734
Bennie Mosher <337539+benniemosher@users.noreply.github.com>
2022-12-10 13:07:21 -0700

    BAM: Added tfcloud workspace config

--- providers.tf
+++ providers.tf
@@ -1,13 +1,12 @@
 terraform {
   required_version = "~> 1.3"
 
-  # cloud {
-  #   organization = "benniemosher-dev"
-  #   workspaces {
-  #     # TODO: Change this to match the workspace name
-  #     name = "terraform-domain-concept"
-  #   }
-  # }
+  cloud {
+    organization = "benniemosher-dev"
+    workspaces {
+      name = "tfcloud-management"
+    }
+  }
 
   required_providers {
     tfe = {
